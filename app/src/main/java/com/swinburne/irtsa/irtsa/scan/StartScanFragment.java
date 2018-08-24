44efff589cbedd8014d19dbc4a8b7acaf960c214
unknown <7678193@student.swin.edu.au>
2018-08-25 04:26:50 +1000

    scan fragments added

---
+++ app/src/main/java/com/swinburne/irtsa/irtsa/scan/StartScanFragment.java
@@ -0,0 +1,67 @@
+package com.swinburne.irtsa.irtsa.scan;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;package com.swinburne.irtsa.irtsamobileapp.scan;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toolbar;
+
+import com.swinburne.irtsa.irtsa.R;
+
+public class StartScanFragment extends Fragment {
+    public StartScanFragment() {
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.toolbar_scan, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_start_scan, container, false);
+
+        initialiseUI(rootView);
+
+        return rootView;
+    }
+
+    private void initialiseUI(View rootView) {
+        Button startScanButton = rootView.findViewById(R.id.startScanButton);
+        startScanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                beginScan();
+            }
+        });
+    }
+
+    private void beginScan() {
+        ViewScanFragment viewScanFragment = new ViewScanFragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        // Store the Fragment in stack
+        transaction.addToBackStack(null);
+        transaction.replace(R.id.scanContainer, viewScanFragment, "ViewScanFragment").commit();
+    }
+
+}
