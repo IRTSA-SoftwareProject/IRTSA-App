f0f35b2f9a654cd98d6dca89c3da43056d63e637
unknown <7678193@student.swin.edu.au>
2018-08-25 04:06:19 +1000

    added ScanContainer class and layout

---
+++ app/src/main/java/com/swinburne/irtsa/irtsa/containers/ScanContainerFragment.java
@@ -0,0 +1,32 @@
+package com.swinburne.irtsa.irtsa.containers;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.swinburne.irtsa.irtsa.R;
+
+public class ScanContainerFragment extends Fragment {
+
+
+    public ScanContainerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scan_container, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+}
