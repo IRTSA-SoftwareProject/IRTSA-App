f1801c09bf6439ca8c3e3b24041e358f08d329e8
unknown <7678193@student.swin.edu.au>
2018-09-10 22:50:33 +1000

    added toolbar icon and other misc tests, commented tests and fix coding standards

--- app/src/androidTest/java/com/swinburne/irtsa/irtsa/TestMainActivity.java
+++ app/src/androidTest/java/com/swinburne/irtsa/irtsa/TestMainActivity.java
@@ -1,16 +1,11 @@
 package com.swinburne.irtsa.irtsa;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,11 +14,6 @@ import org.junit.runner.RunWith;
 public class TestMainActivity {
   @Rule
   public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule(MainActivity.class);
-
-  @Test
-  public void startScanButton_IsVisible() {
-    onView(withId(R.id.startScanButton)).check(matches(isDisplayed()));
-  }
 
   @Test
   public void useAppContext() throws Exception {
--- app/src/androidTest/java/com/swinburne/irtsa/irtsa/TestViewScanFragment.java
+++ app/src/androidTest/java/com/swinburne/irtsa/irtsa/TestViewScanFragment.java
@@ -9,29 +9,63 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 import android.support.test.rule.ActivityTestRule;
-
+import android.support.test.runner.AndroidJUnit4;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+/**
+ * Class to test the UI of the View Scan Fragment.
+ */
+@RunWith(AndroidJUnit4.class)
 public class TestViewScanFragment {
+
+  /**
+   * Launch the MainActivity.
+   */
   @Rule
   public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule(MainActivity.class);
 
+  /**
+   * Navigate to the View Scan Fragment.
+   */
   @Before
   public void setup() {
     onView(withId(R.id.startScanButton)).perform(click());
   }
 
+  /**
+   * Assert the scan image is visible.
+   */
   @Test
   public void image_IsVisible() {
     onView(withId(R.id.scanImage)).check(matches(isDisplayed()));
   }
 
+  /**
+   * Assert the back button hides the View Scan fragment and
+   * displays the Start Scan Fragment.
+   */
   @Test
   public void backPress_NavigatesToStartScanFragment() {
+    onView(withId(R.id.scanImage)).check(matches(isDisplayed()));
+    onView(withId(R.id.startScanButton)).check(doesNotExist());
+
     pressBack();
+
     onView(withId(R.id.scanImage)).check(doesNotExist());
     onView(withId(R.id.startScanButton)).check(matches(isDisplayed()));
+  }
+
+  /**
+   * Assert the toolbar is displaying the correct options.
+   */
+  @Test
+  public void toolbar_HasCorrectOptions() {
+    onView(withId(R.id.save)).check(matches(isDisplayed()));
+    onView(withId(R.id.delete)).check(doesNotExist());
+    onView(withId(R.id.share)).check(doesNotExist());
+    onView(withId(R.id.settings)).check(doesNotExist());
   }
 }
