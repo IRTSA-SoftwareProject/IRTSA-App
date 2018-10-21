29b5127603ba6dc827e9bd7d18bcadd75e10a9d4
Casper-P <100031220@student.swin.edu.au>
2018-10-21 19:39:22 +1100

    test now click on first gallery item in setup

--- app/src/androidTest/java/com/swinburne/irtsa/irtsa/TestGalleryDetailFragment.java
+++ app/src/androidTest/java/com/swinburne/irtsa/irtsa/TestGalleryDetailFragment.java
@@ -1,5 +1,6 @@
 package com.swinburne.irtsa.irtsa;
 
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import org.junit.Before;
@@ -7,16 +8,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
-import static android.support.test.espresso.action.ViewActions.swipeRight;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.assertion.ViewAssertions.selectedDescendantsMatch;
-import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
@@ -32,43 +29,36 @@ public class TestGalleryDetailFragment {
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule(MainActivity.class);
 
     @Before
-    public void setup() {
+    public void Setup() {
         onView(withId(R.id.viewPager)).perform(swipeLeft());
-        onView(withId(R.id.recyclerView)).perform(click());
+        onView(withId(R.id.recyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
     }
+
     @Test
-    public void Gallery_Detail_is_visible()
-    {
+    public void GalleryDetailIsVisible() {
         onView(withId(R.id.galleryDetailImage)).check(matches(isDisplayed()));
         onView(withId(R.id.galleryDetailName)).check(matches(isDisplayed()));
         onView(withId(R.id.galleryDetailDescription)).check(matches(isDisplayed()));
         onView(withId(R.id.galleryDetailDate)).check(matches(isDisplayed()));
-
     }
+
     @Test
-    public void Gallery_Detail_Toolbar_has_correct_option()
-    {
+    public void GalleryDetailToolbarHasCorrectOption() {
         onView(withId(R.id.delete)).check(matches(isDisplayed()));
         onView(withId(R.id.share)).check(matches(isDisplayed()));
     }
 
     @Test
-    public void backPress_navigatesToGalleryFragment() {
-        onView(withId(R.id.recyclerView)).check(doesNotExist());
-        onView(withId(R.id.galleryDetailImage)).check(matches(isDisplayed()));
-        onView(withId(R.id.galleryDetailName)).check(matches(isDisplayed()));
-        onView(withId(R.id.galleryDetailDescription)).check(matches(isDisplayed()));
-        onView(withId(R.id.guideline5)).check(matches(isDisplayed()));
-
+    public void BackPressNavigatesToGalleryFragment() {
         pressBack();
-
         onView(withId(R.id.galleryDetailImage)).check(doesNotExist());
         onView(withId(R.id.galleryDetailName)).check(doesNotExist());
         onView(withId(R.id.galleryDetailDescription)).check(doesNotExist());
         onView(withId(R.id.recyclerView)).check(matches(isDisplayed()));
     }
+
     @Test
-    public void edit_image(){
+    public void EditImage(){
         onView(withId(R.id.galleryDetailImage)).check(matches(isDisplayed()));
         onView(withId(R.id.edit)).perform(click());
         onView(withId(R.id.editName)).check(matches(isDisplayed()));
