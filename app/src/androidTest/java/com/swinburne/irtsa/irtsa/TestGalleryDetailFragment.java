9e1c82b8fad02a4f854b461cd4fa9ce49d52b330
Casper-P <100031220@student.swin.edu.au>
2018-10-21 19:51:36 +1100

    added comments and changed functions to camel case

--- app/src/androidTest/java/com/swinburne/irtsa/irtsa/TestGalleryDetailFragment.java
+++ app/src/androidTest/java/com/swinburne/irtsa/irtsa/TestGalleryDetailFragment.java
@@ -22,46 +22,66 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
  */
 @RunWith(AndroidJUnit4.class)
 public class TestGalleryDetailFragment {
-    /**
-     * Launch the MainActivity.
-     */
-    @Rule
-    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule(MainActivity.class);
 
-    @Before
-    public void Setup() {
-        onView(withId(R.id.viewPager)).perform(swipeLeft());
-        onView(withId(R.id.recyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-    }
+  /**
+   * Launch the MainActivity.
+   */
+  @Rule
+  public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule(MainActivity.class);
 
-    @Test
-    public void GalleryDetailIsVisible() {
-        onView(withId(R.id.galleryDetailImage)).check(matches(isDisplayed()));
-        onView(withId(R.id.galleryDetailName)).check(matches(isDisplayed()));
-        onView(withId(R.id.galleryDetailDescription)).check(matches(isDisplayed()));
-        onView(withId(R.id.galleryDetailDate)).check(matches(isDisplayed()));
-    }
+  /**
+   * Navigate to the GalleryDetailFragment
+   */
+  @Before
+  public void Setup() {
+    onView(withId(R.id.viewPager)).perform(swipeLeft());
+    onView(withId(R.id.recyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+  }
 
-    @Test
-    public void GalleryDetailToolbarHasCorrectOption() {
-        onView(withId(R.id.delete)).check(matches(isDisplayed()));
-        onView(withId(R.id.share)).check(matches(isDisplayed()));
-    }
+  /**
+   * Check if the gallery detail fragment items are visible
+   */
+  @Test
+  public void GalleryDetailIsVisible() {
+    onView(withId(R.id.galleryDetailImage)).check(matches(isDisplayed()));
+    onView(withId(R.id.galleryDetailName)).check(matches(isDisplayed()));
+    onView(withId(R.id.galleryDetailDescription)).check(matches(isDisplayed()));
+    onView(withId(R.id.galleryDetailDate)).check(matches(isDisplayed()));
+  }
 
-    @Test
-    public void BackPressNavigatesToGalleryFragment() {
-        pressBack();
-        onView(withId(R.id.galleryDetailImage)).check(doesNotExist());
-        onView(withId(R.id.galleryDetailName)).check(doesNotExist());
-        onView(withId(R.id.galleryDetailDescription)).check(doesNotExist());
-        onView(withId(R.id.recyclerView)).check(matches(isDisplayed()));
-    }
+  /**
+   * Check the correct toolbar icons are visible
+   */
+  @Test
+  public void GalleryDetailToolbarHasCorrectOption() {
+    onView(withId(R.id.delete)).check(matches(isDisplayed()));
+    onView(withId(R.id.edit)).check(matches(isDisplayed()));
+    onView(withId(R.id.share)).check(matches(isDisplayed()));
+    onView(withId(R.id.save)).check(matches(isDisplayed()));
 
-    @Test
-    public void EditImage(){
-        onView(withId(R.id.galleryDetailImage)).check(matches(isDisplayed()));
-        onView(withId(R.id.edit)).perform(click());
-        onView(withId(R.id.editName)).check(matches(isDisplayed()));
-        onView(withId(R.id.editDescription)).check(matches(isDisplayed()));
-    }
+  }
+
+  /**
+   * Ensure that the GalleryFragment is visible on back press
+   */
+  @Test
+  public void BackPressNavigatesToGalleryFragment() {
+    pressBack();
+    onView(withId(R.id.galleryDetailImage)).check(doesNotExist());
+    onView(withId(R.id.galleryDetailName)).check(doesNotExist());
+    onView(withId(R.id.galleryDetailDescription)).check(doesNotExist());
+    onView(withId(R.id.recyclerView)).check(matches(isDisplayed()));
+  }
+
+  /**
+   * Ensure that the edit image dialog opens when clicking
+   * the edit image toolbar icon
+   */
+  @Test
+  public void EditImage(){
+    onView(withId(R.id.galleryDetailImage)).check(matches(isDisplayed()));
+    onView(withId(R.id.edit)).perform(click());
+    onView(withId(R.id.editName)).check(matches(isDisplayed()));
+    onView(withId(R.id.editDescription)).check(matches(isDisplayed()));
+  }
 }
